03767e2e90702ba2d5d6423585f2abfd960e2c94
Somashekar1996-cpu <somashekarbe1996@gmail.com>
2025-07-14 10:16:59 +0530

    project details added

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,34 +1,34 @@
 const Services_Data = [
     {
-        s_no:"01",
-        s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "01",
+        s_name: "Web design",
+        s_desc: "Web designing is the process of creating the layout, visual appearance, and user experience ..."
     },
     {
-        s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "02",
+        s_name: "Graphics design",
+        s_desc: "Graphic design is the art and practice of creating visual content to communicate messages ..."
     },
     {
-        s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "03",
+        s_name: "Social media",
+        s_desc: "Digital platforms and websites that allow people to create, share, and interact ..."
     },
     {
-        s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "04",
+        s_name: "App design",
+        s_desc: "App design is the process of creating the look, feel, and user experience of a mobile or web application ..."
     },
     {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "05",
+        s_name: "Digital marketing",
+        s_desc: "Digital marketing is the use of the internet and digital technologies to promote products ..."
     },
     {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        s_no: "06",
+        s_name: "Content writing",
+        s_desc: "Content writing is the process of planning, writing, and editing written material for digital platforms ..."
     },
 ]
- 
+
 export default Services_Data;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
             <div id='home' className="hero">
                 <img id='profile_pic' src={profile_img} alt="profile" />
                 <h1><span>I'm Somashekar,</span> frontend developer based in Bangalore, Karnataka.</h1>
-                <p>Enthusiastic and detail-oriented front-end developer with strong foundational knowledge in HTML, CSS, JavaScript, and ReactJS. Proficient to apply web development skills and contribute to user-centric, responsive web applications. </p>
+                <p>Enthusiastic and detail-oriented Front-End Developer with a solid foundation in HTML, CSS, JavaScript, and ReactJS. Passionate about crafting responsive, scalable, user-centric web applications with clean, efficient code. Eager to contribute to dynamic development teams and create engaging digital experiences that prioritize performance and usability. </p>
                 <div className="hero-action">
                     <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
                     <div className="hero-resume">My Resume</div>
